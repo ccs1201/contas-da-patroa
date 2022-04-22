72ee9f13c0c6fe64485dcca68bb9ea414a2e0ef2
start.vaadin.com <no-reply@vaadin.com>
2022-04-22 17:50:00 +0000

    Generated project

---
+++ src/main/java/br/com/daptroa/data/Role.java
@@ -0,0 +1,5 @@
+package br.com.daptroa.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/br/com/daptroa/views/cadastrousuários/CadastroUsuáriosView.java
@@ -0,0 +1,33 @@
+package br.com.daptroa.views.cadastrousuários;
+
+import br.com.daptroa.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import javax.annotation.security.PermitAll;
+
+@PageTitle("Cadastro Usuários")
+@Route(value = "cadastroUsuarios", layout = MainLayout.class)
+@PermitAll
+public class CadastroUsuáriosView extends VerticalLayout {
+
+    public CadastroUsuáriosView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
